aab18b516b96296342f08e31eb39a04e89d714aa
ThomasJ85 <tom.jackson50@gmail.com>
2018-11-06 20:22:32 +0000

    Rearranged files, folder names ; trying to have mainSpec read from main.

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,25 @@
 const displayedCalc = document.getElementById("displayedCalc");
 const displayedInput = document.getElementById("displayedInput");
 
+var addTogether = function(i) {
+	return i + i;
+}
+
 const newNumber = []; 
 let disableDec = false;
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "Dec"];
 for (var key of numbers){
 	let number = document.getElementById("num" + key);
-	number.onclick = function(){
-		
+	number.onclick = function numberBuilder(){
 		   	if (this.value === "." && disableDec == false ){ 
-					newNumber.push(this.value);
-					disableDec = true;
+				disableDec = true;
+				newNumber.push(this.value);
+				console.log(newNumber);
 			} 
 			if(this.value !== ".") { 
 				newNumber.push(this.value); 
-			}
-			
+				console.log(newNumber);
+			}	
 			displayedInput.innerHTML = newNumber.join(""); 
 	}
 }
--- spec/mainSpec.js
+++ spec/mainSpec.js
@@ -1,15 +1,20 @@
-describe("Number Builder", function(){
+describe("mainSpec.js", function(){
+    it("numbers added", function(){
+        expect(addTogether(2)).toBe(4);
+    });
     it("decimal added and decimal button disabled", function(){
-        var disableDec = false;
+        /*addTogether(i);*/
+        expect(number.onclick).toBe(newNumber.push(this.value));
+       /* var disableDec = false;
         var digit = "." /*value of button being pressed*/
-        const newNumber = ["4", "4"];
+       /* const newNumber = ["4", "4"];
         if (digit === "." && disableDec == false) {
             newNumber.push(digit);
             disableDec = true;
         }
         expect(newNumber[newNumber.length - 1]).toBe(".")
-        expect(disableDec).toBe(true)
-    })
+        expect(disableDec).toBe(true)*/
+    });
     it("decimal not added", function(){
         var disableDec = false;
         var digit = "." /*value of button being pressed*/
@@ -17,6 +22,6 @@ describe("Number Builder", function(){
             if (digit === "." && disableDec == false) {
                 newNumber.push(digit);
             }
-        expect(newNumber[newNumber.length - 1]).toBe(".")
-    })
+        expect(newNumber[newNumber.length - 1]).toBe(".");
+    });
  })
