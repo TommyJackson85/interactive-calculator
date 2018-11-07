046c93a151dfd7bbe5136aaa5202851a12fa63c9
ThomasJ85 <tom.jackson50@gmail.com>
2018-11-07 20:05:05 +0000

    Cleaned Jasmine HTML and fixed its file links ; rewrote tests

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,18 @@
 const displayedCalc = document.getElementById("displayedCalc");
 const displayedInput = document.getElementById("displayedInput");
 
-var addTogether = function(i) {
+var addTogether = function(i) { /*temporary basic function for testing*/
 	return i + i;
 }
+console.log(addTogether(3));
 
 const newNumber = []; 
 let disableDec = false;
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "Dec"];
 for (var key of numbers){
-	let number = document.getElementById("num" + key);
+	number = document.getElementById("num" + key);
 	number.onclick = function numberBuilder(){
-		   	if (this.value === "." && disableDec == false ){ 
+		   	if (this.value === "." && disableDec == false){ 
 				disableDec = true;
 				newNumber.push(this.value);
 				console.log(newNumber);
--- spec/mainSpec.js
+++ spec/mainSpec.js
@@ -1,20 +1,28 @@
 describe("mainSpec.js", function(){
     it("numbers added", function(){
         expect(addTogether(2)).toBe(4);
-    });
-    it("decimal added and decimal button disabled", function(){
-        /*addTogether(i);*/
-        expect(number.onclick).toBe(newNumber.push(this.value));
-       /* var disableDec = false;
-        var digit = "." /*value of button being pressed*/
-       /* const newNumber = ["4", "4"];
-        if (digit === "." && disableDec == false) {
-            newNumber.push(digit);
-            disableDec = true;
+    }),
+    it("Number Builder", function(){
+        var newNumber = ["5", "6"];
+        var disableDec = false;
+        numberBuilder(".");
+        function numberBuilder(i){
+            if (i === "." && disableDec == false) { 
+                disableDec = true;
+                expect(disableDec).toBe(true);
+                newNumber.push(i);
+                expect(newNumber[newNumber.length - 1]).toBe(".");
+                numberBuilder("5");
+            } 
+            if (i === "5") {
+                disableDec = false;
+                newNumber.push(i); 
+                expect(disableDec).toBe(false);
+                expect(newNumber[newNumber.length - 1]).toBe("5");
+                expect(newNumber[newNumber.length - 1]).not.toBe(5);
+            }	   
         }
-        expect(newNumber[newNumber.length - 1]).toBe(".")
-        expect(disableDec).toBe(true)*/
-    });
+    }),
     it("decimal not added", function(){
         var disableDec = false;
         var digit = "." /*value of button being pressed*/
@@ -23,5 +31,5 @@ describe("mainSpec.js", function(){
                 newNumber.push(digit);
             }
         expect(newNumber[newNumber.length - 1]).toBe(".");
-    });
+    })
  })
