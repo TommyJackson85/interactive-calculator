285e5693d1efa22b646265d34a0271873ec92b5b
ThomasJ85 <tom.jackson50@gmail.com>
2018-11-08 19:45:20 +0000

    Tested newNumber building process

--- spec/mainSpec.js
+++ spec/mainSpec.js
@@ -1,35 +1,42 @@
-describe("mainSpec.js", function(){
-    it("numbers added", function(){
+describe("newNumber(array) building", function(){
+    it("Basic test works!", function(){
         expect(addTogether(2)).toBe(4);
     }),
-    it("Number Builder", function(){
+    it("Number or decimal added to newNumber array", function(){
         var newNumber = ["5", "6"];
         var disableDec = false;
-        numberBuilder(".");
+        numberBuilder("5");/*tests number string*/
         function numberBuilder(i){
             if (i === "." && disableDec == false) { 
                 disableDec = true;
                 expect(disableDec).toBe(true);
                 newNumber.push(i);
-                expect(newNumber[newNumber.length - 1]).toBe(".");
-                numberBuilder("5");
+                expect(newNumber[newNumber.length - 1]).toBe(".")
             } 
             if (i === "5") {
-                disableDec = false;
                 newNumber.push(i); 
                 expect(disableDec).toBe(false);
                 expect(newNumber[newNumber.length - 1]).toBe("5");
                 expect(newNumber[newNumber.length - 1]).not.toBe(5);
+                numberBuilder(".");/*tests decimal*/
             }	   
+            if (i !== "5" || i !== ".") {
+                    alert("not a number or decimal");
+            }
         }
     }),
-    it("decimal not added", function(){
+    it("Number or decimal NOT added to newNumber array", function(){
+        var newNumber = ["5", "6"];
         var disableDec = false;
-        var digit = "." /*value of button being pressed*/
-        const newNumber = ["4", "4"];
-            if (digit === "." && disableDec == false) {
-                newNumber.push(digit);
-            }
-        expect(newNumber[newNumber.length - 1]).toBe(".");
+        /*tests number string*/
+        function numberBuilder(i){
+            if (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."].includes(i) === false) { 
+                return "not number string or decimal string";
+            } 
+        }
+        expect(newNumber.length).toBe(2);
+        expect(numberBuilder(5)).toBe("not number string or decimal string");
+        expect(numberBuilder("*")).toBe("not number string or decimal string");
+        expect(numberBuilder("-")).toBe("not number string or decimal string");
     })
  })
